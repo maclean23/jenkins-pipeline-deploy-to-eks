6adc951dae69e4506d0345923fdf40e11e0947dc
maclean23 <122225257+maclean23@users.noreply.github.com>
2024-10-08 21:26:40 +0100

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "primuslearning-app"
-    region = "us-east-1"
+    bucket = "learningbucket-app"
+    region = "us-east-2"
     key = "eks/terraform.tfstate"
   }
 }
